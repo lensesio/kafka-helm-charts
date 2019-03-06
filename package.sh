05ce846d1df33775cf3537759be082d3d5ca3b4c
Spiros Economakis <spiros.oikonomakis@gmail.com>
2019-03-06 09:53:59 +0200

    Changes for repository versioning and helmignore
    - Automatically publish in repos the charts
    - Add `.helmignore` to avoid including files which are not needed for
    packaging
    - Move packages to `gh-pages` branch
    - Optimise scripts for CI/CD
    - Keep multiple versions for each chart

--- package.sh
+++ package.sh
@@ -1,50 +1,24 @@
 #!/usr/bin/env bash
 set -o errexit
+set -o pipefail
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 GREEN='\033[0;32m'
 ARTIFACTORY_URL=https://landoop.github.io/kafka-helm-charts/
-CHARTS=$(find charts -maxdepth 1 -mindepth 1 -type d)
-HELM_VERSION='v2.5.1'
 
-mkdir -p packages
-rm -rf packages/*.tgz
-
-echo "Cleaning and preparing"
-find . -iname "*.tgz" -type f | grep -v 'docs/' | xargs rm
-
-echo "Linting and checking"
-scripts/lint.sh
-RET=$?
-
-# if [[ "${RET}" == 1 ]]; then
-#     exit 1
-# fi  
+# Fetch packages
+git clone -b gh-pages https://github.com/landoop/kafka-helm-charts.git build
 
 echo "Packaging charts..."
-cd packages && helm package ../charts/*
+# Iterate over all charts are package them
+for dir in ${TRAVIS_BUILD_DIR}/charts/*; do
+    helm dep update $dir
+    helm package -d ${TRAVIS_BUILD_DIR}/build $dir
+done
 
-cd ../
-echo "Checking version compatibility"
-scripts/test.sh
-RET=0
-
-if [[ "${RET}" == 0 ]]; then 
-    #echo "Merging index.yaml"
-    #curl "$ARTIFACTORY_URL/index.yaml" -o index.yaml --fail -sSL -m 5
-    helm repo index packages --url=$ARTIFACTORY_URL #--merge=index.yaml
-
-    echo "Copying chart packages and index to docs"
-    rm -f docs/*.tgz
-    rm -f docs/*.yaml
-    rm -f packages/*.compare
-    cp packages/*.tgz docs/
-    cp packages/index.yaml docs/
-
-    echo "${GREEN}Now commit and push charts and docs!${NC}"
+# Indexing of charts
+if [ -f index.yaml ]; then
+    helm repo index --url ${ARTIFACTORY_URL} --merge index.yaml ${TRAVIS_BUILD_DIR}/build
 else
-    echo "${RED}Tests failed, charts not packaged!${NC}"
+    helm repo index --url ${ARTIFACTORY_URL} ${TRAVIS_BUILD_DIR}/build
 fi
-
-rm -fr packages
+echo "${GREEN}Success!${NC}"
